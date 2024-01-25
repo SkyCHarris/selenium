0fc54513bac9969b8329ba6d2f11ea47686cfc5d
Sky Harris <skyler.iqeq@gmail.com>
2024-01-24 18:50:50 -0700

    Working program to open, wait, find, and click cookie 5000x

---
+++ cookie_clicker_working.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+
+
+driver = webdriver.Chrome()
+driver.get("https://orteil.dashnet.org/cookieclicker/")
+
+driver.implicitly_wait(5)
+
+choose_lang = driver.find_element(By.ID, "langSelect-EN" )
+choose_lang.click()
+
+cookie = driver.find_element(By.ID, "bigCookie")
+cookie_count = driver.find_element(By.ID, "cookies")
+
+actions = ActionChains(driver)  # actionchains object, works like a queue
+
+for i in range(5000):
+    actions.perform()   # performs above actions (click cookie)
+    actions.click(cookie) # click mouse wherever it's located
+
+#TODO: FKIN WORKS LEZZZ GOOOOO
